0d23d4d9724d68f8bf72cb4d6d25aaa13ffb42e0
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-10-30 11:53:17 -0300

    :arrow_up: upgrade dependencies

--- dist/vue-uuid.cjs.js
+++ dist/vue-uuid.cjs.js
@@ -8,11 +8,15 @@ var v1 = _interopDefault(require('uuid/v1'));
 var v4 = _interopDefault(require('uuid/v4'));
 var v5 = _interopDefault(require('uuid/v5'));
 
-var uuid = { v1: v1, v4: v4, v5: v5 };
+var uuid = {
+  v1: v1,
+  v4: v4,
+  v5: v5
+};
 
-var install = function (Vue) {
+var install = function install(Vue) {
   Vue.prototype.$uuid = uuid;
 };
 
 exports.uuid = uuid;
-exports['default'] = install;
+exports.default = install;
--- dist/vue-uuid.es.js
+++ dist/vue-uuid.es.js
@@ -2,11 +2,15 @@ import v1 from 'uuid/v1';
 import v4 from 'uuid/v4';
 import v5 from 'uuid/v5';
 
-var uuid = { v1: v1, v4: v4, v5: v5 };
+var uuid = {
+  v1: v1,
+  v4: v4,
+  v5: v5
+};
 
-var install = function (Vue) {
+var install = function install(Vue) {
   Vue.prototype.$uuid = uuid;
 };
 
-export { uuid };
 export default install;
+export { uuid };
