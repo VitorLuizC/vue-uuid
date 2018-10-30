2b09e5df6a5f37a99d05cddb7189ab6e634f66fb
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-10-30 12:50:42 -0300

    :bookmark: bump to version 1.1.0

--- dist/vue-uuid.js
+++ dist/vue-uuid.js
@@ -1,5 +1,5 @@
 /*!
- * vue-uuid v1.0.0
+ * vue-uuid v1.1.0
  * (c) 2017-present Vitor Luiz Cavalcanti
  * Released under the MIT License.
  */
@@ -355,11 +355,38 @@
 
 	var v5 = v35('v5', 0x50, sha1Browser);
 
+	/**
+	 * @typedef {{ v1: typeof v1, v4: typeof v4, v5: typeof v5 }} UUID
+	 */
+
+	/**
+	 * An object with uuid's v1, v4 and v5 functions.
+	 * @type {UUI}
+	 */
+
 	var uuid = {
 	  v1: v1_1,
 	  v4: v4_1,
 	  v5: v5
 	};
+	/**
+	 * Installs UUID on Vue instance. It creates a property on Vue instance to
+	 * expose an object with uuid's v1, v4 and v5 functions.
+	 * @example ```js
+	 * import Vue from 'vue';
+	 * import VueUUID from 'vue-uuid';
+	 *
+	 * Vue.use(VueUUID);
+	 *
+	 * new Vue({
+	 *   mounted () {
+	 *     console.log(this.$uuid.v1());
+	 *   }
+	 * });
+	 * ```
+	 * @param {import('vue').default} Vue Vue constructor.
+	 */
+
 	function install(Vue) {
 	  Vue.prototype.$uuid = uuid;
 	}
